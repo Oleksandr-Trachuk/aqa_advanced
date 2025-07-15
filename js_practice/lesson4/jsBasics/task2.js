c4e33ce465bd6e7a3e519df4feeef9800f77a440
Oleksandr Trachuk <oleksandrtraczuk@gmail.com>
2025-07-15 13:25:52 +0200

    Add task files for lesson 4

---
+++ js_practice/lesson4/jsBasics/task2.js
@@ -0,0 +1,10 @@
+const name1 = "Oleksandr";
+const name2 = "Anna";
+
+// Конкатенація
+const greetingConcat = "Привіт, " + name1 + " і " + name2 + "!";
+console.log(greetingConcat);
+
+// Шаблонний рядок
+const greetingTemplate = `Привіт, ${name1} і ${name2}!`;
+console.log(greetingTemplate);
